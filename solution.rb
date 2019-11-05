ec224ccff8f3d22099c3191f32cc0a682330435b
dianadhu <46409024+dianadhu@users.noreply.github.com>
2019-11-04 20:34:53 -0500

    Update solution.rb

--- solution.rb
+++ solution.rb
@@ -1,29 +1,15 @@
 require 'sinatra'
-require 'rubygems'
 
 get '/' do
-<<<<<<< HEAD
-	erb :dijo
+	erb :index
 end
 
-get '/answer' do
-	said = params[:say]
-	@str = ""
-	if said.match(said.upcase) == nil
-		@str = "Habla mas duro mijito"
-	else
-		@str = "Ahhh si, manzanas!"
+post '/abuelita' do
+	if params[:queDijo] == params[:queDijo].upcase && params[:queDijo] != "" 
+		"Ahhh si, manzanas!"
+		else
+		<<-HTML
+		<h1>Habla más duro mijito</h1>
+		HTML
 	end
-	erb :codigo
-=======
-  erb :index
-end
-
-get '/respuesta' do
-  if params[:resp]==params[:resp].upcase
-  erb :upcase
-  else
-  erb :others
-  end
->>>>>>> 1978eee026dea29c79d8ad2c071f7170b4c7045f
 end
