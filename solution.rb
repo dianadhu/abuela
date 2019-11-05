8e45432ffeb01c32460b969d062688e1ce338594
Dianadhu <dimidusa@gmail.com>
2019-11-04 19:56:40 -0500

    Código

---
+++ solution.rb
@@ -0,0 +1,15 @@
+require 'sinatra'
+
+get '/' do
+	erb :codigo
+end
+
+post '/abuelita' do
+	if params[:queDijo] == params[:queDijo].upcase && params[:queDijo] != ""
+		"Ahhh si, manzanas!"
+		else
+		<<-HTML
+		<h1>Habla más duro mijito</h1>
+		HTML
+	end
+end
